1d5145d6b0c0236ab6c999affb8b3a7feab0dd9b
CU <cuahn001@gmail.com>
2026-04-29 16:25:03 +0800

    Add files via upload

---
+++ transaction_model.py
@@ -0,0 +1,12 @@
+from pydantic import BaseModel
+from typing import Optional
+
+class TransactionModel(BaseModel):
+    id: int
+    timestamp: int
+    category: str
+    amount: float
+    tag: str
+
+    linked_budget: Optional[str] = "None"
+    
